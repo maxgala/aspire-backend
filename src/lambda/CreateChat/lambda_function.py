b4fef8af13be699c28c6e0de4ebe66d6727ab355
Saima Ali <saiima.ali@mail.utoronto.ca>
2020-06-13 13:24:52 -0400

    created a simple handler

--- src/lambda/CreateChat/lambda_function.py
+++ src/lambda/CreateChat/lambda_function.py
@@ -1,18 +1,16 @@
 import json
 from chat import *
 import inspect
-
-import sys
+from base import Session, engine, Base
 
 def handler(event, context):
     session = Session()
     
     #create a new chat instance
-
     chat = Chat()
-    chat_attribs = inspect.getmembers(Chat, lambda x: not(inspect.isroutine(x)))
+    chat_attribs = dir(Chat)
     
     for attrib in chat_attribs:
-        setattr(chat, attrib, event[attrib])
-    
-handler({1: "test"})
+        if not (attrib.startswith('_') or attrib.strip() == "metadata"):
+            #print(attrib)
+            setattr(chat, attrib, event[attrib])
