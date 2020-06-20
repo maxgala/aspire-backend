61d5f55f81521915bf9e63cbb7c39fbf23fc056f
Saima Ali <saiima.ali@mail.utoronto.ca>
2020-06-20 13:08:39 -0400

    added get chat by id

---
+++ src/lambda/Chat/GetChatById/lambda_function.py
@@ -0,0 +1,39 @@
+import json
+from chat import *
+from base import Session
+from datetime import datetime
+
+def handler(event, context):
+    info = json.loads(event["body"])
+    chat_id = info["chat_id"]
+
+    session = Session()
+    chat = session.query(Chat).get(chat_id)
+    session.close()
+
+    chat_attribs = []
+    pruned_attribs = ["chat_id"]
+
+    for attrib in dir(Chat):
+        if not (attrib.startswith('_') or attrib.strip() == "metadata"\
+                or attrib in pruned_attribs):
+            chat_attrib.append(attrib)
+    
+    if chat != None:
+        # construct JSON-able dictionary
+        chat_dict = {}
+        for attrib in chat_attribs:
+            chat_dict[attrib] = str(getattr(chat, attrib)) 
+        return {"statusCode": 200,
+                "body": json.dumps({
+                    chat_dict
+                })
+        }
+    
+    else:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({
+                "message": "ID {} not found in Chats table".format(chat_id)
+            })
+        }
