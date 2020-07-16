a211b4f4ab0a80a08c72f14b5c4c5ac2c507804d
Saleh Bakhit <saleh.bakhit@hotmail.com>
2020-07-15 22:54:36 -0400

    auth layer, updated template.yaml (not active yet: commented out)

--- src/lambda/IndustryTags/DeleteById/lambda_function.py
+++ src/lambda/IndustryTags/DeleteById/lambda_function.py
@@ -3,12 +3,23 @@ import logging
 
 from industry_tag import IndustryTag
 from base import Session, engine, Base
+# from role_validation import UserGroups, assume_role, get_group, validate_group
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
+    # # check authorization
+    # err, group_response = validate_group(event['requestContext']['authorizer']['claims'], [UserGroups.ADMIN])
+    # if err:
+    #     return {
+    #         "statusCode": 401,
+    #         "body": json.dumps({
+    #             "errorMessage": group_response
+    #         })
+    #     }
+
     industryTagId = event["pathParameters"].get("industryTagId") if event["pathParameters"] else None
     if not industryTagId:
         return {
