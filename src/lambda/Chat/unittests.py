f2f572a78d2b8d4bfc1d4615fc6caeb748e3e5fe
Saima Ali <saiima.ali@mail.utoronto.ca>
2020-08-08 10:51:08 -0400

    modified template to point chat endpoints to aspireapi

---
+++ src/lambda/Chat/unittests.py
@@ -0,0 +1,10 @@
+import unittest
+
+import CreateChat as create
+import DeleteChatById as delete
+import EditChatById as edit
+import GetAllChats as get_all
+import GetChatById as get
+import ReserveChatById as reserve
+import UnreserveChatById as unreserve
+
