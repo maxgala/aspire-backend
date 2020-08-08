fb143ebab9c380ca33074537d966dee4bbef0d80
Saima Ali <saiima.ali@mail.utoronto.ca>
2020-08-08 13:15:21 -0400

    added 4 tests for create chat

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -14,12 +14,13 @@ from GetChatById import lambda_function as get
 from ReserveChatById import lambda_function as reserve
 from UnreserveChatById import lambda_function as unreserve
 
+import chat
 
 context = ""
 
 class TestCreateChat(unittest.TestCase):
-    self.msg_status_code = "Expected status code{}, but returned {}"
-    self.body_status_code = "Expected body code{}, but returned {}"
+    msg_status_code = "Expected status code{}, but returned {}"
+    msg_body = "Expected body {}, but returned {}"
     def test00_one_on_one(self):
         event = {}
         request = { "chat_type": 1,
@@ -27,21 +28,84 @@ class TestCreateChat(unittest.TestCase):
                     "aspiring_professionals": []
                     }
         
-        event["body"] = request
+        event["body"] = json.dumps(request)
+        
         actual = create.handler(event, context)
         
-        expected = {"statusCode": 200, "body": request}
-        self.assertEqual(actual["statusCode"], expected["statusCode"], self.msg_status_code.format(expected["statusCode"], \
-                                                                                  actual["statusCode"]))
-        self.assertEqual(actual["body"], expected["body"], self.msg_body_code.format(actual["body"], expected["body"])
+        self.assertEqual(actual["statusCode"], 200, \
+                         self.msg_status_code.format(200, \
+                                                     actual["statusCode"]))
+
         
-    def test01_four_on_one_no_date(self):
-        pass
-    def test02_four_on_one_with_date(self):
-        pass
-    def test03_mock_interview(self):
-        pass
+    def test01_one_on_four_no_date(self):
+        event = {}
+        request = { "chat_type": 2,
+                    "senior_executive": "larry@gmail.com",
+                    "aspiring_professionals": []
+                    }
+
+        event["body"] = json.dumps(request)
+        actual = create.handler(event, context)
+        expected = {"statusCode": 400, \
+                    "body": {"message": "For a {} chat, date must be specified".format(chat.ChatType.ONE_ON_FOUR)
+                    }}
+        
+        self.assertEqual(actual["statusCode"], expected["statusCode"], \
+                         self.msg_status_code.format(expected["statusCode"], \
+                                                     actual["statusCode"]))
+        actual_body = actual["body"]
+        assert (json.loads(actual_body) == expected["body"])
+
+        event = {}
+        request = { "chat_type": 2,
+                    "senior_executive": "larry@gmail.com",
+                    "aspiring_professionals": [],
+                    "date": 10000
+                    }
+        
+        event["body"] = json.dumps(request)
+        actual = create.handler(event, context)
+
+        expected = {"statusCode": 200, "body": request}
+        self.assertEqual(actual["statusCode"], expected["statusCode"], \
+                         self.msg_status_code.format(expected["statusCode"], \
+                                                     actual["statusCode"]))
+        
+    def test03_mock_interview_no_date(self):
+        event = {}
+        request = { "chat_type": 3,
+                    "senior_executive": "larry@gmail.com",
+                    "aspiring_professionals": []
+                    }
+        event["body"] = json.dumps(request)
+        
+        actual = create.handler(event, context)
+        expected = {"statusCode": 400, \
+                    "body": {"message": "For a {} chat, date must be specified".format(chat.ChatType.MOCK_INTERVIEW)
+                    }}
+        
+        self.assertEqual(actual["statusCode"], expected["statusCode"], \
+                         self.msg_status_code.format(expected["statusCode"], \
+                                                     actual["statusCode"]))
+
+        actual_body = actual["body"]
+        assert (json.loads(actual_body) == expected["body"])
     
+    def test03_mock_interview_with_date(self):
+        event = {}
+        request = { "chat_type":3,
+                    "senior_executive": "larry@gmail.com",
+                    "aspiring_professionals": [],
+                    "date": 10000
+                    }
+        event["body"] = json.dumps(request)
+        
+        actual = create.handler(event, context)
+
+        expected = {"statusCode": 200, "body": request}
+        self.assertEqual(actual["statusCode"], expected["statusCode"], \
+                         self.msg_status_code.format(expected["statusCode"], \
+                                                     actual["statusCode"]))
 
 if __name__ == "__main__":
     unittest.main(exit=False)
