9c470c15548d36baea44aba3ac7465f4c64dc6bd
Shehryar <shehryarsuleman@gmail.com>
2020-09-09 13:29:55 -0400

    add dataloader lambda func

--- scheduler/data_loader.py
+++ scheduler/data_loader.py
@@ -1,67 +1,72 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
+import csv
 import datetime
+import json
+from scheduler import *
 
+from base import Session, MutableList
+from sqlalchemy.types import BigInteger
 FILEPATH = './sample/data.csv'
-
-
 def renameColumns(data):
     return data.rename(columns={
         'Frequency': 'frequency',
         'date': 'fixed_dates',
         'senior_executive': 'email'
     }, inplace=True)
+# def cleanData(data):
+#     return data.replace(np.nan, "", inplace=True)
+def loadData(session):
+    with open(FILEPATH, 'r') as csv_file:
+        data = csv.DictReader(csv_file, delimiter=',')
+        #renameColumns(data)
+        # cleanData(data)
+        # create new data
+        new_data = []
+        for row in data:
+            fixed_dates = list(filter(lambda date: len(date) > 0,
+                                      row['date'].split(",")))
+            num_dates_autoassign = int(row['Frequency']) - len(fixed_dates)
+            if num_dates_autoassign < 0:
+                raise ValueError("Invalid data in row ", row)
+            chat_index = 0
+            # place fixed dates first
+            for date in fixed_dates:
+                new_data.append({
+                    'id': row['senior_executive'] + str(chat_index),
+                    'email': row['senior_executive'],
+                    'type': row['chat_type'],
+                    'end_date': (datetime.strptime(date, '%Y/%m/%d') - datetime(2020, 1, 1)).days,
+                    'booked': False,
+                })
+                chat_index += 1
+            # assign dates spaced out 365/num_dates_autoassign
+            date_idx = 0
+            space_interval = 365 // num_dates_autoassign
+            while date_idx < num_dates_autoassign:
+                new_data.append({
+                    'id': row['senior_executive'] + str(chat_index),
+                    'email': row['senior_executive'],
+                    'type': row['chat_type'],
+                    'end_date': date_idx * space_interval,
+                    'booked': False,
+                })
+                chat_index += 1
+                date_idx += 1
 
-
-def cleanData(data):
-    return data.replace(np.nan, "", inplace=True)
-
-
-def loadData():
-    data = pd.read_csv(FILEPATH)
-
-    renameColumns(data)
-    cleanData(data)
-
-    # create new data
-    new_data = []
-
-    for index, row in data.iterrows():
-        fixed_dates = list(filter(lambda date: len(date) > 0,
-                                  row['fixed_dates'].split(",")))
-        num_dates_autoassign = row['frequency'] - len(fixed_dates)
-
-        if num_dates_autoassign < 0:
-            raise ValueError("Invalid data in row ", row)
-
-        chat_index = 0
-
-        # place fixed dates first
-        for date in fixed_dates:
-            new_data.append({
-                'id': row['email'] + str(chat_index),
-                'email': row['email'],
-                'type': row['chat_type'],
-                'end_date': (datetime.datetime.strptime(date, '%Y/%m/%d') - datetime.datetime(2020, 1, 1)).days,
-                'booked': False,
-            })
-            chat_index += 1
-
-        # assign dates spaced out 365/num_dates_autoassign
-        date_idx = 0
-        space_interval = 365 // num_dates_autoassign
-        while date_idx < num_dates_autoassign:
-            new_data.append({
-                'id': row['email'] + str(chat_index),
-                'email': row['email'],
-                'type': row['chat_type'],
-                'end_date': date_idx * space_interval,
-                'booked': False,
-            })
-            chat_index += 1
-            date_idx += 1
-
+    for x in new_data:
+        if x['type'] == '1on1':
+            x['type'] = 'ONE_ON_ONE'
+        row = Scheduler(chat_type = ChatType[x['type']],email = x['email'],end_date = x['end_date'], chat_status = ChatStatus.ACTIVE)
+        session.add(row)
     print(new_data)
 
+def handler(event, context):
 
-loadData()
+    # FOR REFERENCE
+    # # create a new session
+    session = Session()
+    session.execute('''TRUNCATE TABLE scheduler''')
+    loadData(session)
+    session.commit()
+    session.close()
--- src/lambda/CreateDatabaseModels/lambda_function.py
+++ src/lambda/CreateDatabaseModels/lambda_function.py
@@ -5,6 +5,7 @@ from chat import Chat, ChatType, ChatStatus
 from job import Job, JobType, JobStatus, JobTags
 from job_application import JobApplication, JobApplicationStatus
 from industry_tag import IndustryTag
+from scheduler import Scheduler
 from base import Session, engine, Base
 
 logger = logging.getLogger()
