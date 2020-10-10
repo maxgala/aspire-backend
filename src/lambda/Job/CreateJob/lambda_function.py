cd670ff2d1fea43c6246e45758b4d62e1ba5db2e
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2020-10-09 23:09:16 -0400

    Use ID token to get user attributes

--- src/lambda/Job/CreateJob/lambda_function.py
+++ src/lambda/Job/CreateJob/lambda_function.py
@@ -9,83 +9,43 @@ from job import Job, JobType, JobStatus, JobTags
 from job_application import JobApplication, JobApplicationStatus
 from base import Session, engine, Base, row2dict
 import boto3
-
-client = boto3.client('cognito-idp')
+import jwt
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    id_token = (event['headers']['Authorization']).split('Bearer ')[1]
+    user = jwt.decode(id_token, verify=False)
 
-    # FOR REFERENCE
-    # # create a new session
     session = Session()
-    
-    """
-    #Commented out this authentication because we are planning to do some other way in the future
-    try:
-        access_token = (event['headers']['Authorization']).replace('Bearer ', '')
-    except:
-        return {
-        "statusCode": 401,
-        "body": json.dumps({
-            "message": "Authorization header is expected"
-        }),
-    }
-    
-    getuserresponse =client.get_user(
-            AccessToken=access_token
-        )
-    
-    user_att=getuserresponse['UserAttributes']
-    mem_type = ''
-    for att in user_att:
-        if att['Name'] == 'custom:user_type':
-            user_type = att['Value']
-        elif att['Name'] == 'email':
-            email = att['Value']
-        elif att['Name'] == 'family_name':
-            family_name = att['Value']
-        elif att['Name'] == 'given_name':
-            given_name = att['Value']
-        try:
-            if att['Name'] == 'custom:membership_type':
-                mem_type = att['Value']
-        except:
-            pass
+
+    user_type = user['custom:user_type']
+    mem_type = user['custom:membership_type']
     
     if user_type == 'Mentor' or (user_type == 'Mentee' and mem_type == 'premium'):
-    """
-    info = json.loads(event["body"])
-    
-    tags = []
-    for tag in info["job_tags"]:
-        tags.append(JobTags[tag])
-    # # create job
-    Job_row = Job(title=info["title"], company=info["company"],
-                    region=info["region"], city=info["city"], country=info["country"], job_type=JobType[info["job_type"]],
-                    description=info["description"], requirements=info["requirements"], posted_by=info["posted_by"],
-                    poster_family_name = info["poster_family_name"], poster_given_name = info["poster_given_name"],
-                    job_status=JobStatus[info["job_status"]] if "job_status" in info else "OPEN",job_tags=tags, salary=info["salary"], deadline = info["deadline"])
+        info = json.loads(event["body"])
+        
+        tags = []
+        for tag in info["job_tags"]:
+            tags.append(JobTags[tag])
 
-    # # persists data
-    session.add(Job_row)
-    
+        Job_row = Job(title=info["title"], company=info["company"],
+                        region=info["region"], city=info["city"], country=info["country"], job_type=JobType[info["job_type"]],
+                        description=info["description"], requirements=info["requirements"], posted_by=info["posted_by"],
+                        poster_family_name = info["poster_family_name"], poster_given_name = info["poster_given_name"],
+                        job_status=JobStatus[info["job_status"]] if "job_status" in info else "OPEN",job_tags=tags, salary=info["salary"], deadline = info["deadline"])
 
-    # # commit and close session
-    
-    session.commit()
-    
-
-    return {
-        "statusCode": 201,
-        "body": json.dumps({
-            "message": "Created Job Row",
-            "job": row2dict(Job_row)
-        }),
-    }
-    session.close()
-    """
+        session.add(Job_row)        
+        session.commit()
+        session.close()
+        return {
+            "statusCode": 201,
+            "body": json.dumps({
+                "message": "Created Job Row",
+                "job": row2dict(Job_row)
+            }),
+        }
     else:
         return {
             "statusCode": 426,
@@ -93,4 +53,3 @@ def handler(event, context):
                 "message": "You are not allowed to post a Job. Upgrade your membership"
             }),
         }
-    """
