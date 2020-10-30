8fb8d31473f59bdef65b0a529b4ada19f1cbbe89
Saleh Bakhit <saleh.bakhit@hotmail.com>
2020-10-29 22:44:49 -0400

    CreateIndustryTags update auth

--- src/lambda/IndustryTags/Create/lambda_function.py
+++ src/lambda/IndustryTags/Create/lambda_function.py
@@ -16,7 +16,7 @@ def handler(event, context):
         UserGroups.PAID,
         UserGroups.FREE
     ]
-    success, _ = read_auth(event['headers']['Authorization'], authorized_groups)
+    success, _ = check_auth(event['headers']['Authorization'], authorized_groups)
     if not success:
         return {
             "statusCode": 401,
