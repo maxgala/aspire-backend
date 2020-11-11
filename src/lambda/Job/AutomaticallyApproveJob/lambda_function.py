01d3c17975db0934a5ffd4386aff487f72bcfa5f
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2020-11-11 16:34:08 -0500

    Implement logic to flip status after 24 hours

--- src/lambda/Job/AutomaticallyApproveJob/lambda_function.py
+++ src/lambda/Job/AutomaticallyApproveJob/lambda_function.py
@@ -1,44 +1,30 @@
-import sys
-import json
 import logging
-import uuid
 from datetime import datetime
 
-from job import Job, JobType, JobStatus, JobTags
-from job_application import JobApplication, JobApplicationStatus
-from base import Session, engine, Base, row2dict
-from role_validation import UserGroups, check_auth
+from job import Job
+from base import Session
 from send_email import send_email
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    logger.info('NABA IS HERE')
+    logger.info('## BEGIN')
+    logger.info('Starting to approve jobs posted > 24 hours ago')
 
-    """session = Session()
-
-    info = json.loads(event["body"])
-
-    tags = []
-    for tag in info["job_tags"]:
-        tags.append(JobTags[tag])
-
-    Job_row = Job(title=info["title"], company=info["company"],
-                    region=info["region"], city=info["city"], country=info["country"], job_type=JobType[info["job_type"]],
-                    description=info["description"], requirements=info["requirements"], posted_by=info["posted_by"],
-                    poster_family_name = info["poster_family_name"], poster_given_name = info["poster_given_name"],
-                    job_status="UNDER_REVIEW",job_tags=tags, salary=info["salary"], deadline = info["deadline"])
-
-    session.add(Job_row)        
+    session = Session()
+    jobs = session.query(Job).filter(Job.job_status == 'UNDER_REVIEW')
+    num_seconds_24_hours = 24*60*60
+    for job in jobs:
+        if datetime.now().timestamp() - job.created_on >= num_seconds_24_hours:
+            job.job_status = 'ACTIVE'
+            job_title = job.title
+            today = datetime.today().strftime("%Y-%m-%d")
+            hiring_manager = job.posted_by    
+            subject = "[MAX Aspire] Your job is now active!"
+            body = f"Salaam!\r\n\nWe are delighted to confirm that the job posting {job_title} is successfully posted on MAX Aspire job board on {today}. You will frequently be notified about the job applications as and when received. Keep an eye on your member portal/email.\r\n\nWe appreciate you putting your trust in MAX Aspire. We wish you luck in hiring the best possible candidate form our talented pool of aspiring professionals.\r\n\nBest regards,\nTeam MAX Aspire\r\n"
+            send_email(to_addresses=hiring_manager, subject=subject, body_text=body)
     session.commit()
-    res = row2dict(Job_row)
     session.close()
 
-    job_title = info["title"]
-    today = datetime.today().strftime("%Y-%m-%d")
-    hiring_manager = info["posted_by"]    
-    subject = "[MAX Aspire] The job you posted is under review"
-    body = f"Salaam!\r\n\nThank you for choosing MAX Aspire as your entrusted partner. I am delighted to confirm that we have received your job posting {job_title} on {today}. A member of our team is currently reviewing the job posting to make sure it meets all the necessary requirements.\r\n\nOnce approved, you will be notified via email/member portal. Subsequently you will be notified if any additional information is required. Kindly bear with us 2-3 days before the job is approved and posted on MAX Aspire Job Board. Thank you.\r\n\nBest regards,\nTeam MAX Aspire\r\n"
-    send_email(to_addresses=hiring_manager, subject=subject, body_text=body)
-"""
+    logger.info('## END')
