1f3f87dd229fd8257a062d9f256c7d11a269a35f
Saleh Bakhit <saleh.bakhit@hotmail.com>
2020-12-05 18:58:27 -0500

    cleanup

--- src/models/base.py
+++ src/models/base.py
@@ -4,41 +4,10 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 
-load_dotenv()
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.ext.mutable import Mutable
 
-# class MutableList(Mutable, list):
-#     ''' Use case: when the col type is ARRAY
-#     and is expected to change.
-
-#     To create a new entry:
-#     l = [...]
-#     Table.Col = MutableList.coerce(Table.Col, l)
-
-#     To update an existing entry, treat it as a
-#     typical Pythonic list:
-#     Table.Col.append(val)
-#     '''
-#     @classmethod # call MutableList.coerce()
-#     def coerce(cls, key, value):
-#         if not isinstance(value, MutableList):
-#             if isinstance(value, list):
-#                 return MutableList(value)
-#             return Mutable.coerce(key, value)
-#         else:
-#             return value
-
-#     def append(self, value):
-#         list.append(self, value)
-#         self.changed()
-#     def pop(self, index=0):
-#         value = list.pop(self, index)
-#         self.changed()
-#         return value
 
 def row2dict(row):
     d = {}
@@ -59,6 +28,7 @@ def row2dict(row):
         d[key] = val
     return d
 
+load_dotenv()
 rdsHost= os.getenv('RDS_HOST')
 rdsPort= os.getenv('RDS_PORT')
 rdsUsername= os.getenv('RDS_USERNAME')
