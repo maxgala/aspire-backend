6cd491c69cab63bc369c26d6b6e40c0a65a5b9bb
Saleh Bakhit <saleh.bakhit@hotmail.com>
2020-12-05 14:49:23 -0500

    Chat[Un]Reserve: use cognito layer

--- src/lambda/Chat/ReserveById/lambda_function.py
+++ src/lambda/Chat/ReserveById/lambda_function.py
@@ -1,20 +1,17 @@
 import json
 import logging
-import boto3
 from botocore.exceptions import ClientError
 from datetime import datetime, date, timedelta, time
 
 from chat import Chat, ChatType, ChatStatus, credit_mapping
 from base import Session
 from role_validation import UserGroups, check_auth
-from cognito_helpers import admin_update_credits
+from cognito_helpers import get_users, admin_update_credits
 from send_email import send_email, build_calendar_invite
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-client = boto3.client('cognito-idp')
-USER_POOL_ID = 'us-east-1_OiH5DGpGX'
 
 def handler(event, context):
     # check authorization
@@ -82,7 +79,6 @@ def handler(event, context):
                 "errorMessage": "user '{}' does not have sufficient credits to reserve chat with id '{}'".format(user['email'], chatId)
             })
         }
-    admin_update_credits(user['email'], (-credit_mapping[chat.chat_type]))
 
     if chat.chat_type == ChatType.FOUR_ON_ONE:
         if len(chat.aspiring_professionals) > 0:
@@ -100,6 +96,8 @@ def handler(event, context):
         if chat.chat_status == ChatStatus.RESERVED:
             prepare_and_send_emails(chat)
     except ClientError as e:
+        session.rollback()
+        session.close()
         logging.info(e)
         if int(e.response['ResponseMetadata']['HTTPStatusCode']) >= 500:
             return {
@@ -110,81 +108,52 @@ def handler(event, context):
                 "statusCode": 400
             }
     else:
+        admin_update_credits(user['email'], (-credit_mapping[chat.chat_type]))
+
         session.commit()
         session.close()
-
         return {
             "statusCode": 200
         }
 
 def prepare_and_send_emails(chat):
-    mentee_IDs = chat.aspiring_professionals 
-    mentor_ID = chat.senior_executive 
+    mentee_IDs = chat.aspiring_professionals
+    mentor_ID = chat.senior_executive
 
     event_name = 'MAX Aspire Coffee Chat'
     event_description = chat.description
 
-    print("before any date or time")
-
     if chat.fixed_date:
-        print("chat has a fixed date")
         chat_date = chat.fixed_date
     else:
-        print("chat is undated")
         today = date.today()
         day_idx = (today.weekday() + 1) % 7 # today.weekday() is 0 for Monday
         chat_date = today + timedelta(days=7-day_idx)
     chat_time = time(14,0,0)
     event_start = datetime.combine(chat_date,chat_time)
-
-    # event_start = datetime(2020,12,4,9,0,0) #chat.fixed_date.time(9,0,0)
-    # chat_date = datetime(2020,12,4) #chat.fixed_date
-    chat_date = f'{chat_date:%b %d, %Y}' 
-    print(chat_date)
+    chat_date = f'{chat_date:%b %d, %Y}'
     event_end = event_start + timedelta(hours=12)
-    
-    mentor = client.admin_get_user(
-        UserPoolId = USER_POOL_ID,
-        Username = mentor_ID
-    )
 
     mentees = []
     for m in mentee_IDs:
-        try: 
-            m_User = client.admin_get_user(
-                UserPoolId = USER_POOL_ID,
-                Username = m
-            )
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-        mentees.append(get_full_name(m_User))
+        m_User, _ = get_users(filter_=("email", m), attributes_filter=["given_name", "family_name"])
+        mentees.append("%s %s" % (m_User['attributes']['given_name'], m_User['attributes']['family_name']))
 
-    mentor_name = get_full_name(mentor)
+    mentor, _ = get_users(filter_=("email", mentor_ID), attributes_filter=["given_name", "family_name"])
+    mentor_name = "%s %s" % (mentor['attributes']['given_name'], mentor['attributes']['family_name'])
     mentee_name = f"{*mentees,}"
-
     if chat.chat_type == ChatType.FOUR_ON_ONE:
         subject = '[MAX Aspire] 4 on 1 coffee chat confirming the 4 attendees'
         mentee_body = f"Salaam!\nWe are delighted to confirm your 4 on 1 coffee chat with {mentor_name}.\nYour coffee chat will take place on: {chat_date}\n\nPlease connect with the Senior Executive to find a time that works for both of you.\nPlease make sure of your attendance. In case of any changes in the circumstances contact the support team at your earliest.\n\nBest regards,\n\nThe MAX Aspire Team"
-
         mentor_body = f"Salaam {mentor_name}!\n\nWe are delighted to confirm your 4 on 1 coffee chat with {mentee_name}.\n\nYour coffee chat will take place on: {chat_date}\n\nPlease connect with the Aspiring Professionals to find a time that works for both of you.\n\nIn case of any changes in the circumstances contact the support team at your earliest.\n\nBest regards,\n\nThe MAX Aspire Team"
     else:
         subject = '[MAX Aspire] 1 on 1 coffee chat'
         mentee_body = f"Salaam!\n\nWe are delighted to confirm your 1 on 1 coffee chat with {mentor_name}.\n\nYour coffee chat will take place on: {chat_date}\n\nPlease connect with the Senior Executive to find a time that works for both of you.\n\nPlease make sure of your attendance. In case of any changes in the circumstances contact the support team at your earliest.\n\nBest regards,\n\nThe MAX Aspire Team"
-
         mentor_body = f"Salaam {mentor_name}!\n\nWe are delighted to confirm your 1 on 1 coffee chat with {mentee_name}.\n\nYour coffee chat will take place on: {chat_date}\n\nPlease connect with the Aspiring Professional to find a time that works for both of you.\n\nIn case of any changes in the circumstances contact the support team at your earliest.\n\nBest regards,\n\nThe MAX Aspire Team"
 
     all_attendees = mentee_IDs.copy().append(mentor_ID)
     ics = build_calendar_invite(event_name, event_description, event_start, event_end, all_attendees)
     send_email(mentee_IDs, subject, mentee_body, ics=ics)
-    send_email(mentor_ID, subject, mentor_body, ics=ics)
-
-def get_full_name(user):
-    user_data = user['UserAttributes']
-    given = ''
-    family = ''
-    for i in user_data:
-        if i['Name'] == 'given_name':
-            given = i['Value']
-        elif i['Name'] == 'family_name':
-            family = i['Value']
-    return given + " " + family
+    # TODO: send to test_mentor_1@maxgala.com
+    # send_email(mentor_ID, subject, mentor_body, ics=ics)
+    send_email('test_mentor_1@maxgala.com', subject, mentor_body, ics=ics)
--- src/lambda/Chat/UnreserveById/lambda_function.py
+++ src/lambda/Chat/UnreserveById/lambda_function.py
@@ -1,19 +1,16 @@
 import json
 import logging
-import boto3
 from botocore.exceptions import ClientError
 
 from chat import Chat, ChatType, ChatStatus, credit_mapping
 from base import Session
 from role_validation import UserGroups, check_auth
-from cognito_helpers import admin_update_credits
+from cognito_helpers import get_users, admin_update_credits
 from send_email import send_email
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-client = boto3.client('cognito-idp')
-USER_POOL_ID = 'us-east-1_OiH5DGpGX'
 
 def handler(event, context):
     # check authorization
@@ -73,9 +70,6 @@ def handler(event, context):
             })
         }
 
-    print('aspiring_professionals 1')
-    print(chat.aspiring_professionals)
-
     aspiring_professionals = chat.aspiring_professionals
     chat.aspiring_professionals = None
     for ap in aspiring_professionals:
@@ -85,100 +79,49 @@ def handler(event, context):
             else:
                 chat.aspiring_professionals = [ap]
 
-    prepare_and_send_email_to_ap(user['email'], chat.senior_executive)
-    prepare_and_send_email_to_se(user['email'], chat.senior_executive)
-
-    admin_update_credits(user['email'], credit_mapping[chat.chat_type])
-
     if (chat.chat_type != ChatType.ONE_ON_ONE and chat.chat_status == ChatStatus.RESERVED) \
         or (chat.chat_type != ChatType.FOUR_ON_ONE and (chat.aspiring_professionals is None \
         or len(chat.aspiring_professionals) == 0)):
         chat.chat_status = ChatStatus.ACTIVE
 
+    try:
+        prepare_and_send_email_to_ap(user['email'], chat.senior_executive)
+        prepare_and_send_email_to_se(user['email'], chat.senior_executive)
+    except ClientError as e:
+        session.rollback()
+        session.close()
+        logging.info(e)
+        if int(e.response['ResponseMetadata']['HTTPStatusCode']) >= 500:
+            return {
+                "statusCode": 500
+            }
+        else:
+            return {
+                "statusCode": 400
+            }
+    else:
+        admin_update_credits(user['email'], credit_mapping[chat.chat_type])
 
-    session.commit()
-    session.close()
-
-    return {
-        "statusCode": 200
-    }
-
+        session.commit()
+        session.close()
+        return {
+            "statusCode": 200
+        }
 
 def prepare_and_send_email_to_ap(ap, se):
-
-    mentor = client.admin_get_user(
-        UserPoolId = USER_POOL_ID,
-        Username = se
-    )
-
-    mentor_name = get_full_name(mentor)
+    mentor, _ = get_users(filter_=("email", se), attributes_filter=["given_name", "family_name"])
+    mentor_name = "%s %s" % (mentor['attributes']['given_name'], mentor['attributes']['family_name'])
 
     subject = '[MAX Aspire] Coffee chat unreserved'
-    mentee_body = f"Salaam,\n\nWe have received your cancellation request, and thus can confirm that your reserved coffee chat with the Senior Executive {mentor_name} is now cancelled.\n\nPlease note that any credits spent on the coffee chat are non refundable. You can login to your account to purchase credits and book any future coffee chats.\n\nThank you.\n\nBest regards,\n\nThe MAX Aspire Team"
-
-    try:
-        send_email(ap, subject, mentee_body)
-    except Exception as e:
-        print(e)
+    mentee_body = f"Salaam,\n\nWe have received your cancellation request, and thus can confirm that your reserved coffee chat with the Senior Executive {mentor_name} is now cancelled.\n\nYou can login to your account to purchase credits and book any future coffee chats.\n\nThank you.\n\nBest regards,\n\nThe MAX Aspire Team"
+    send_email(ap, subject, mentee_body)
 
 def prepare_and_send_email_to_se(ap, se):
-
-    mentee = client.admin_get_user(
-        UserPoolId = USER_POOL_ID,
-        Username = ap
-    )
-
-    mentee_name = get_full_name(mentee)
+    mentee, _ = get_users(filter_=("email", ap), attributes_filter=["given_name", "family_name"])
+    mentee_name = "%s %s" % (mentee['attributes']['given_name'], mentee['attributes']['family_name'])
 
     subject = '[MAX Aspire] Coffee chat unreserved'
     mentor_body = f"Salaam,\n\nWe have received your cancellation request, and thus can confirm that your reserved coffee chat with the Aspiring Professional(s) {mentee_name} is now cancelled.\n\nThank you.\n\nBest regards,\n\nThe MAX Aspire Team"
-
-    send_email(se, subject, mentor_body)
-    try:
-        send_email(se, subject, mentor_body)
-    except Exception as e:
-        print(e)
-
-def prepare_and_send_email(chat):
-
-    # print('starting the unreserve script')
-    # print(chat)
-
-    mentee_IDs = chat.aspiring_professionals 
-    mentor_ID = chat.senior_executive 
-
-    mentor = client.admin_get_user(
-        UserPoolId = USER_POOL_ID,
-        Username = mentor_ID
-    )
-
-    mentees = []
-    for m in mentee_IDs:
-        try: 
-            m_User = client.admin_get_user(
-                UserPoolId = USER_POOL_ID,
-                Username = m
-            )
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-        mentees.append(get_full_name(m_User))
-
-    mentor_name = get_full_name(mentor)
-    mentee_name = f"{*mentees,}"
-
-    subject = '[MAX Aspire] Coffee chat unreserved'
-    mentee_body = f"Salaam,\n\nWe have received your cancellation request, and thus can confirm that your reserved coffee chat with the Senior Executive {mentor_name} is now cancelled.\n\nYou can login to your account to book any future coffee chats.\n\nThank you.\n\nBest regards,\n\nThe MAX Aspire Team"
-
-    mentor_body = f"Salaam,\n\nWe have received your cancellation request, and thus can confirm that your reserved coffee chat with the Aspiring Professional(s) {mentee_name} is now cancelled.\n\nThank you.\n\nBest regards,\n\nThe MAX Aspire Team"
-
-    send_email(mentee_IDs, subject, mentee_body)
-    send_email(mentor_ID, subject, mentor_body)
-
-def get_full_name(user):
-    user_data = user['UserAttributes']
-    for i in user_data:
-        if i['Name'] == 'given_name':
-            given = i['Value']
-        elif i['Name'] == 'family_name':
-            family = i['Value']
-    return given + " " + family
+    # TODO: send to test_mentor_1@maxgala.com
+    # send_email(se, subject, mentor_body)
+    send_email('test_mentor_1@maxgala.com', subject, mentor_body)
