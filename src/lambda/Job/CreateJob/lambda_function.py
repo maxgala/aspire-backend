3fce0a6ecb4edf5e24e75a8edbaf916fd2f4c460
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2020-12-24 01:04:08 -0500

    Update email copy

--- src/lambda/Job/CreateJob/lambda_function.py
+++ src/lambda/Job/CreateJob/lambda_function.py
@@ -33,9 +33,11 @@ def handler(event, context):
     for tag in info["job_tags"]:
         tags.append(JobTags[tag])
 
+    job_title = info["title"]
+
     deadline = datetime.fromtimestamp(info["deadline"]).replace(hour=0, minute=0,second=0, microsecond=0)
 
-    Job_row = Job(title=info["title"], company=info["company"],
+    Job_row = Job(title=job_title, company=info["company"],
                     region=info["region"], city=info["city"], country=info["country"], job_type=JobType[info["job_type"]],
                     description=info["description"], requirements=info["requirements"], posted_by=info["posted_by"],
                     poster_family_name = info["poster_family_name"], poster_given_name = info["poster_given_name"],
@@ -49,9 +51,9 @@ def handler(event, context):
     ##email hiring manager
     job_title = info["title"]
     today = datetime.today().strftime("%Y-%m-%d")
-    hiring_manager = info["posted_by"]    
-    subject = "[MAX Aspire] The job you posted is under review"
-    body = f"Salaam!\r\n\nThank you for choosing MAX Aspire as your entrusted partner. I am delighted to confirm that we have received your job posting {job_title} on {today}. A member of our team is currently reviewing the job posting to make sure it meets all the necessary requirements.\r\n\nOnce approved, you will be notified via email/member portal. Subsequently you will be notified if any additional information is required. Kindly bear with us 2-3 days before the job is approved and posted on MAX Aspire Job Board. Thank you.\r\n\nBest regards,\nTeam MAX Aspire\r\n"
+    hiring_manager = info["posted_by"]   
+    subject = f"[MAX Aspire] Your {job_title} job is under review"
+    body = f"Salaam!\n\nThank you for choosing MAX Aspire as your entrusted partner. I am delighted to confirm that we have received your job posting {job_title} on {today}.\n\nYour Job Posting will be reviewed and if approved, will go LIVE within 36 hours.\n\nYou will be notified by email as individuals apply for the position.\n\nThank you.\n\nKind Regards,\nTeam MAX Aspire"
     send_email(to_addresses=hiring_manager, subject=subject, body_text=body)
 
     return http_status.success(json.dumps(res))
