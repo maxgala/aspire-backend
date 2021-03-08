0c1ae87c6abcb8f43778a88caca9b098a2b4aed5
Naba S. Siddiqui <naba.sadia.siddiqui@gmail.com>
2021-03-07 23:25:10 -0500

    Debug

--- src/layers/database/database_layer_packages.sh
+++ src/layers/database/database_layer_packages.sh
@@ -5,9 +5,8 @@ export PKG_DIR="${BASE_DIR}/python"
 
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
 
-python --version
-which python
-python3.6 -m pip install -r requirements.txt --no-deps -t ${PKG_DIR}
+python3 --version
+python -m pip install -r requirements.txt --no-deps -t ${PKG_DIR}
 touch ${PKG_DIR}/__init__.py
 cp ../../models/*.py ${PKG_DIR}
 cp ../../models/.env ${PKG_DIR}
