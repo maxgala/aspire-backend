70bcfc810422ad6261bd39a8a4cc99b528a1ebec
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2021-03-12 23:20:09 -0500

    Convert to string

--- src/auth/role_validation.py
+++ src/auth/role_validation.py
@@ -18,7 +18,7 @@ def check_auth(auth_header: str, allowedUserTypes: list):
 
     user = jwt.decode(id_token, verify=False)
     success, user_type = validate_user_type(user)
-    logging.info('user_type is ' + user_type)
+    logging.info('user_type is ' + str(user_type))
     if not success:
         return False, None
     if user_type not in allowedUserTypes:
