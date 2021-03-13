1934edf52bc2a1c4c620da1c376615a5717b3182
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2021-03-12 21:51:21 -0500

    Update email templates

--- src/lambda/JobApplications/CreateJobApplication/lambda_function.py
+++ src/lambda/JobApplications/CreateJobApplication/lambda_function.py
@@ -78,7 +78,7 @@ def handler(event, context):
     }
     template_data = json.dumps(template_data)
     recipients = [hiring_manager]
-    send_templated_email(recipients, "CreateJobApplication", template_data)
+    send_templated_email(recipients, "JobApplication-Creation-HiringManager", template_data)
 
     template_data = {
         "job_title": str(job_title),
@@ -86,7 +86,7 @@ def handler(event, context):
     }
     template_data = json.dumps(template_data)
     recipients = [email]
-    send_templated_email(recipients, "CreateJobApplication-Applicant", template_data)
+    send_templated_email(recipients, "JobApplication-Creation-Applicant", template_data)
 
     resp = row2dict(job_rs)
     session.close()
