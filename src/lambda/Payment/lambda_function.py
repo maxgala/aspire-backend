e6c57049523dc9447625466b548263866611318e
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2021-03-13 13:31:44 -0500

    Send email receipt

--- src/lambda/Payment/lambda_function.py
+++ src/lambda/Payment/lambda_function.py
@@ -15,19 +15,21 @@ def handler(event, context):
 
     try:
         req_body = json.loads(event["body"])
-        if "payment_method_id" in req_body:
+        if "payment_method_id" in req_body and "amount" in req_body and "email" in req_body:
             _amount = int(float(req_body["amount"]) * 100)
+            _email = req_body["email"]
             intent = stripe.PaymentIntent.create(
                 payment_method = req_body["payment_method_id"],
                 amount = _amount,
                 currency = 'cad',
                 confirmation_method = 'automatic',
                 confirm = True,
-                payment_method_types=["card"]
+                payment_method_types=["card"],
+                receipt_email = _email
                 )
             return generate_response(intent)
         else:
-            return http_status.bad_request('Payment failed')
+            return http_status.bad_request('Missing required attributes: payment_method_id, amount, email')
     except Exception as e:
         return http_status.bad_request('Payment failed. '+ str(e))
 
