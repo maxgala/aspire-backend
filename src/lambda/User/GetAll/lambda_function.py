410de816ccd6eb6fbd90c4ff3f780e82249dad23
Naba Sadia Siddiqui <NabaSadiaSiddiqui@users.noreply.github.com>
2021-03-13 21:15:28 -0500

    Update lambda_function.py

--- src/lambda/User/GetAll/lambda_function.py
+++ src/lambda/User/GetAll/lambda_function.py
@@ -22,7 +22,8 @@ def handler(event, context):
         user_type = None
 
     users, count = get_users(filter_=filter_, user_type=user_type)
-    _users = users.values()
+    print(users)
+    _users = list(users.values())
     print(_users)
     return http_status.success(json.dumps({
             "users": _users,
